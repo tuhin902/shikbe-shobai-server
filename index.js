c03e5cdeabeddc196f309f3ff390bfa80a258f4f
tuhin902 <tuhin902md@gmail.com>
2022-10-26 20:38:45 +0600

    categories ready for website

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 4000;
 
+const categories = require('./data/categories.json');
+
 app.get('/', (req, res) => {
     res.send('course api running');
 });
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories);
+})
 
 app.listen(port, () => {
     console.log('course api running on port', 4000);
