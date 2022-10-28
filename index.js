e991107e87a62a2fe2d25df39113a73542431a6c
tuhin902 <tuhin902md@gmail.com>
2022-10-28 19:51:22 +0600

    Final commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 4000;
 
+app.use(cors());
+
 const categories = require('./data/categories.json');
+const courseinfo = require('./data/course.json');
 
 app.get('/', (req, res) => {
     res.send('course api running');
@@ -12,6 +16,14 @@ app.get('/course-categories', (req, res) => {
     res.send(categories);
 })
 
+app.get('/courseinfo/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courseinfo.find((n => n.id === id));
+    res.send(selectedCourse);
+})
+
+
+
 app.listen(port, () => {
-    console.log('course api running on port', 4000);
+    console.log('course api running on port', port);
 });
